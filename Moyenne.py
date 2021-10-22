5ede797b856718df46b83e504f959688716d00bd
Notzzed <48843323+Notzzed@users.noreply.github.com>
2021-10-22 11:43:52 +0200

    Create Moyenne.py

---
+++ Moyenne.py
@@ -0,0 +1,5 @@
+Valeur1=float(input("La première valeur est : "))
+Valeur2=float(input("La deuxième valeur est : "))
+Valeur3=float(input("La troisième valeur est : "))
+Moyenne=(Valeur1+Valeur2+Valeur3)/3
+print("La moyenne est de : ", Moyenne)

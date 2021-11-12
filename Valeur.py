64e020eae0981f6497a9b61f1ede0a770e68c305
Notzzed <48843323+Notzzed@users.noreply.github.com>
2021-11-12 12:11:22 +0100

    Create Valeur.py

---
+++ Valeur.py
@@ -0,0 +1,6 @@
+a=int(input("Rentrez la 1ère valeur"))
+b=int(input("Rentrez la 2ème valeur"))
+if (a>b):
+    print("La plus grande valeur vaut",a)
+else:
+    print("La plus grande valeur vaut",b)

32fe9241c1685ee3609e1a97db83688216926986
Notzzed <48843323+Notzzed@users.noreply.github.com>
2021-12-03 11:28:50 +0100

    Create affiche_mois.py

---
+++ affiche_mois.py
@@ -0,0 +1,9 @@
+def affiche_mois(numero):
+    if numero == 1:
+        print("Nous sommes en janvier.")
+    if numero == 2:
+        print("Nous sommes en février.")
+    if numero == 3:
+        print("Nous sommes en mars.")
+    # etc.
+    return
